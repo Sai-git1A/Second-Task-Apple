980684a4cf7b439dd4e961789938e5e726ef940c
Sai-git1A <saidharshan018@gmail.com>
2022-05-02 16:45:58 +0530

    Apple

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
+// Popup
 var popup = document.getElementById("popup");
-var sec = document.querySelector(".sec");
-var click = document.getElementById("btn");
-var box = document.getElementById("dark")
 
 setTimeout(function() {
   window.onload(popupopen());
-}, 1000);
+}, 500);
 
 function popupopen() {
   popup.style="display: block";
@@ -13,17 +11,54 @@ function popupopen() {
 
 function popupclose() {
   popup.style="display: none";
+  getLocation()
 }
+
+// Dark Mode
+var sec = document.querySelector(".sec");
+var click = document.getElementById("btn");
+var box = document.getElementById("dark")
 
 function mode() {
   sec.classList.toggle("dark")
   if (click.innerHTML === "Dark") {
     click.innerHTML = "Light";
-    click.style="color: white";
-    box.style="background-color: black;border: 1px solid white;"
+    click.style="color: black";
+    box.style="background-color: white;border: 1px solid black"
   } else {
     click.innerHTML = "Dark";
-    click.style="color: black";
-    box.style="background-color: white;border: 1px solid black;"
+    click.style="color: white";
+    box.style="background-color: black;border: 1px solid white"
   }
+}
+
+// Location
+var cityName = document.getElementById("city");
+
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(latlong);
+  } else {
+    cityName.innerHTML = "Not found.";
+  }
+}
+
+function latlong(position) {
+  var url = "https://api.openweathermap.org/data/2.5/weather?lat=" + position.coords.latitude + "&lon=" + position.coords.longitude + "&appid=3326b4fff200d14de2ee14b0590940e1";
+  fetch(url)
+     .then(response => response.json())
+     .then(data => weatherData(data));
+}
+
+function weatherData(data) {
+  var weatherTemp = document.getElementById("weather");
+  var weatherIcon = document.getElementById("img");
+  const name = data.name;
+  const temp = data.main.temp;
+  const icon = data.weather[0].icon;
+
+  cityName.innerHTML = name;
+  weatherTemp.innerHTML = Math.floor(temp - 273.15) + "°C";
+  var iconURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+  weatherIcon.src = iconURL;
 }
